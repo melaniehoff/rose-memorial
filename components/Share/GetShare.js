4eb11f3c6dbcf27534abcb2c80e4e139e4cb50e8
CommonMatter <hoffmelanie@gmail.com>
2020-04-28 21:49:27 -0400

    router share form breaking

---
+++ components/Share/GetShare.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import '../Components.css';
+
+class GetShare extends Component {
+
+
+  render() {
+
+        return(
+            <div id="share_content">
+            </div>
+        );
+    }
+    }
+
+  export default GetShare;
