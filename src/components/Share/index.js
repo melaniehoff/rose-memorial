735f059f8afb85d2c416a8fbfb536b533448a6c7
cofuente <contact@cofuente.io>
2020-05-14 13:57:47 -0400

    refactor for clarity and simplicity

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import Airtable from 'airtable'
-import request from 'superagent';
-import { photosUploaded, updateUploadedPhoto } from '../../actions';
-import P5Wrapper from 'react-p5-wrapper';
-import sketch from '../../utils/p5/sketch';
+import React, { Component } from "react";
+import Airtable from "airtable";
+import request from "superagent";
+import { photosUploaded, updateUploadedPhoto } from "../../actions";
+import P5Wrapper from "react-p5-wrapper";
+import sketch from "../../utils/p5/sketch";
 
-const base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('appZuPErukOoOExF9');
+const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_API_KEY }).base("appZuPErukOoOExF9");
+
 class Share extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dedication: '',
-      optional_note:'',
-      optional_photo:'',
-      rose_id:'',
+      dedication: "",
+      optional_note: "",
+      optional_photo: "",
+      rose_id: "",
       onUpdateUploadedPhoto: updateUploadedPhoto,
-      onPhotosUploaded: photosUploaded
+      onPhotosUploaded: photosUploaded,
     };
     this.handleDedication = this.handleDedication.bind(this);
     this.handleNote = this.handleNote.bind(this);
@@ -24,131 +25,136 @@ class Share extends Component {
   }
 
   handleDedication(event) {
-    this.setState({dedication: event.target.value});
+    this.setState({ dedication: event.target.value });
   }
 
   handleNote(event) {
-    this.setState({optional_note: event.target.value});
+    this.setState({ optional_note: event.target.value });
   }
 
   handlePhoto(event) {
-    this.setState({optional_photo: event.target.value});
+    this.setState({ optional_photo: event.target.value });
   }
-
 
   onPhotoSelected(files) {
-        const url = `https://api.cloudinary.com/v1_1/${
-            process.env.REACT_APP_CLOUD_NAME
-            }/upload`;
-    
+    const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/upload`;
 
-        for (let file of files) {
-            const photoId = this.photoId++;
-            const fileName = file.name;
+    for (let file of files) {
+      const photoId = this.photoId++;
+      const fileName = file.name;
 
-            request.post(url)
-                .field('upload_preset', process.env.REACT_APP_PRESET_NAME)
-                .field('file', file)
-                .field('multiple','false')
-                .on('progress', (progress) => this.onPhotoUploadProgress(photoId, file.name, progress))
-                .end((error, response) => {
-                    this.onPhotoUploaded(photoId, fileName, response);
-                });
-        }
-    }
-   onPhotoUploadProgress(id, fileName, progress) {
-        this.state.onUpdateUploadedPhoto({
-            id: id,
-            fileName: fileName,
-            progress: progress,
+      request
+        .post(url)
+        .field("upload_preset", process.env.REACT_APP_PRESET_NAME)
+        .field("file", file)
+        .field("multiple", "false")
+        .on("progress", (progress) =>
+          this.onPhotoUploadProgress(photoId, file.name, progress)
+        )
+        .end((error, response) => {
+          this.onPhotoUploaded(photoId, fileName, response);
         });
     }
-
-    onPhotoUploaded(id, fileName, response) {
-        this.state.onUpdateUploadedPhoto({
-            id: id,
-            fileName: fileName,
-            response: response,
-        });
-
-        this.state.onPhotosUploaded([response.body]);
-        this.setState({optional_photo: response.body['url']});
-
-    }
-  handleSubmit(event) {
-
-    //save roses in cloudinary
-  const dataURI = document.getElementsByTagName("canvas")[0].getAttribute("data-uri");
-  const url = `https://api.cloudinary.com/v1_1/${
-        process.env.REACT_APP_CLOUD_NAME
-        }/upload`;
-
-        request.post(url)
-            .field('upload_preset', process.env.REACT_APP_PRESET_NAME)
-            .field('file', dataURI)
-            .field('multiple','false')
-            .end((error, response) => {
-                console.log(response)
-                this.setState({rose_id: [response.body["public_id"],response.body["delete_token"]]})
-                
-                //send info to airtable 
-                  const {dedication, optional_note, optional_photo, rose_id} = this.state
-                  base('RoseGarden').create([
-                    {
-                      "fields": {
-                        "Timestamp": Date.now(),
-                        "Dedication": dedication,
-                        "OptionalNote": optional_note,
-                        "OptionalPhoto": [{"url": optional_photo}],
-                        "RoseSVG": [{"url": response.body["url"]}],
-                        "Public": "Yes"
-                      }
-                    }
-                  ], function(err, records) {
-                    if (err) {
-                      console.error(err);
-                      return;
-                    }
-                    document.getElementById('submission').classList.add("submitted")
-
-                    setTimeout(function(){
-
-                            const url = `https://@api.cloudinary.com/v1_1/${
-                                  process.env.REACT_APP_CLOUD_NAME
-                                  }/delete_by_token`;
-                          
-                                  request.post(url)
-                                      .field('token', rose_id[1])
-                                      .end((error, response) => {
-                                         if(error){
-                                            console.log('done deletting')
-                                         }else{
-                                          console.log(error)
-                                         }
-                                          
-                                      });
-                            document.getElementById('submission').classList.add("submitted")
-
-                    },2000)
-                  });
-            });
-
-
-    event.preventDefault();
- 
   }
 
+  onPhotoUploadProgress(id, fileName, progress) {
+    this.state.onUpdateUploadedPhoto({
+      id: id,
+      fileName: fileName,
+      progress: progress,
+    });
+  }
+
+  onPhotoUploaded(id, fileName, response) {
+    this.state.onUpdateUploadedPhoto({
+      id: id,
+      fileName: fileName,
+      response: response,
+    });
+
+    this.state.onPhotosUploaded([response.body]);
+    this.setState({ optional_photo: response.body["url"] });
+  }
+  handleSubmit(event) {
+    //save roses in cloudinary
+    const dataURI = document
+      .getElementsByTagName("canvas")[0]
+      .getAttribute("data-uri");
+    const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/upload`;
+
+    request
+      .post(url)
+      .field("upload_preset", process.env.REACT_APP_PRESET_NAME)
+      .field("file", dataURI)
+      .field("multiple", "false")
+      .end((error, response) => {
+        // console.log(response);
+        this.setState({
+          rose_id: [response.body["public_id"], response.body["delete_token"]],
+        });
+
+        //send info to airtable
+        const {
+          dedication,
+          optional_note,
+          optional_photo,
+          rose_id,
+        } = this.state;
+
+        const submissionCallBack = (err, records) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          document.getElementById("submission").classList.add("submitted");
+          const timeoutCallBack = () => {
+            const url = `https://@api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/delete_by_token`;
+            request
+              .post(url)
+              .field("token", rose_id[1])
+              .end((error, response) => {
+                if (error) {
+                  console.log("done deletting");
+                } else {
+                  console.log(error);
+                }
+              });
+            document.getElementById("submission").classList.add("submitted");
+          };
+          setTimeout(timeoutCallBack, 2000);
+        };
+        const submissionArray = [
+          {
+            fields: {
+              Timestamp: Date.now(),
+              Dedication: dedication,
+              OptionalNote: optional_note,
+              OptionalPhoto: [{ url: optional_photo }],
+              RoseSVG: [{ url: response.body["url"] }],
+              Public: "Yes",
+            },
+          },
+        ];
+        base("RoseGarden").create(submissionArray, submissionCallBack);
+      });
+
+    event.preventDefault();
+  }
 
   render() {
-  	const {dedication, optional_note} = this.state; 
+    const { dedication, optional_note } = this.state;
     return (
       <React.Fragment>
-        <div id='flower'></div>
+        <div id="flower"></div>
         <P5Wrapper sketch={sketch} />
-        <form id='submission' onSubmit={this.handleSubmit}>
+        <form id="submission" onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text" value={dedication} onChange={this.handleDedication} />
+            <input
+              type="text"
+              value={dedication}
+              onChange={this.handleDedication}
+            />
           </label>
 
           <label>
@@ -158,30 +164,21 @@ class Share extends Component {
 
           <label>
             Optional Photo:
-          
             <input
-                type="file"
-                id="fileupload"
-                accept="image/*"
-                multiple={false}
-                ref={fileInputEl =>
-                    (this.fileInputEl = fileInputEl)
-                }
-                onChange={() =>
-                    this.onPhotoSelected(
-                        this.fileInputEl.files
-                    )
-                }
+              type="file"
+              id="fileupload"
+              accept="image/*"
+              multiple={false}
+              ref={(fileInputEl) => (this.fileInputEl = fileInputEl)}
+              onChange={() => this.onPhotoSelected(this.fileInputEl.files)}
             />
           </label>
-
 
           <input type="submit" value="Submit" />
         </form>
       </React.Fragment>
     );
   }
-
- }
+}
 
 export default Share;
