bbe2fe0a99f3230093c6550dbe759be983bcf0a9
Zainab Aliyu <zoaliyu@gmail.com>
2020-06-01 15:47:00 -0400

    minor

--- src/components/RoseWall/index.js
+++ src/components/RoseWall/index.js
@@ -39,23 +39,26 @@ class RoseWall extends Component {
         <div className="rose-content">
       {this.state.rosesHere ? roses :
         <div className="info">
-        <p>
-          <span>
-          Welcome to the CLOUD9 Memorial Garden, a space for collective remembrance with care, gentleness and respect.
-          This time is especially challenging because we are limited in how we can gather to mourn and remember.
-          We recognize creating intentional space to grieve and celebrate collective memory as vital   during this period of mass loss and isolation.
+          <div className="info-text">
+            <p>
+              <span>
+              Welcome to the CLOUD9 Memorial Garden, a space for collective remembrance with care, gentleness and respect.
+              This time is especially challenging because we are limited in how we can gather to mourn and remember.
+              We recognize creating intentional space to grieve and celebrate collective memory as vital   during this period of mass loss and isolation.
 
-          </span>
-          <span>
-          This is a continuation and extension of CLOUD9 (Collective Love on Ur Desktop), as our community continues to plant new seeds and mourn all
-          of Ours that we have lost in this time, due to state violence, due to Covid and due to the continued systems centered on harming Black people,
-          Indigenous people, brown people, low income people, trans people, undocumented people, and marginalized people.
-          </span>
-          <span>
-          We invite you to plant seeds of liberation to transform our collective grief.
-          </span>
-        </p>
-        <button id="modal-button" onClick={this.clickHandler}> Enter Garden</button>
+              </span>
+              <span>
+              This is a continuation and extension of <a href="https://cloud9.support/">CLOUD9 (Collective Love on Ur Desktop)</a>, as our community continues to plant new seeds and mourn all
+              of Ours that we have lost in this time, due to state violence, due to Covid and due to the continued systems centered on harming Black people,
+              Indigenous people, brown people, low income people, trans people, undocumented people, and marginalized people.
+              </span>
+              <span>
+              We invite you to plant seeds of liberation to transform our collective grief.
+              </span>
+
+            <button id="modal-button" onClick={this.clickHandler}> Enter Garden</button>
+            </p>
+        </div>
       </div>
 
       }
