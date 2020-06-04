fcaffbe0bc3d57f53cd00e63eaa41d925fde3b17
Zainab Aliyu <zoaliyu@gmail.com>
2020-06-04 11:53:36 -0400

    leave space for ritual

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -27,7 +27,7 @@ class Share extends Component {
   clickHandler() {
       document.getElementById('submission').classList.add('show');
       this.setState({formHere: true})
-      document.getElementById("modal-button").style.display = "none";    
+      document.getElementById("modal-button").style.display = "none";
   }
   handleDedication(event) {
     this.setState({ dedication: event.target.value });
@@ -172,22 +172,11 @@ class Share extends Component {
 
 
         <div className="info">
-      
+
           <div className="info-text">
             <p className="medium-text">
               <span>
-              Welcome to the CLOUD9 Memorial Garden, a space for collective remembrance with care, gentleness and respect.
-              This time is especially challenging because we are limited in how we can gather to mourn and remember.
-              We recognize creating intentional space to grieve and celebrate collective memory as vital   during this period of mass loss and isolation.
-
-              </span>
-              <span>
-              This is a continuation and extension of <a href="https://cloud9.support/">CLOUD9 (Collective Love on Ur Desktop)</a>, as our community continues to plant new seeds and mourn all
-              of Ours that we have lost in this time, due to state violence, due to Covid and due to the continued systems centered on harming Black people,
-              Indigenous people, brown people, low income people, trans people, undocumented people, and marginalized people.
-              </span>
-              <span>
-              We invite you to plant seeds of liberation to transform our collective grief.
+                content for ritual and time to pause before submitting
               </span>
 
             <button className="medium-text" id="modal-button" onClick={this.clickHandler}> Submit a Dedication</button>
