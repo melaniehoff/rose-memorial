cd77f3a5c114e5e52883deb66d0ac5bb452f1abb
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-06-27 16:00:55 -0400

    adds stars.js

---
+++ public/stars.js
@@ -0,0 +1,33 @@
+document.ready(function(){
+setInterval(function makeDiv(){
+    var divsize = ((Math.random()*2) + 1).toFixed();
+    var color = ((Math.random()*255) + 100).toFixed();
+    $newdiv = $('<div/>').css({
+        'width':divsize+'px',
+        'height':divsize+'px',
+        'background-color': "rgba(" + color +"," +color +","+color+",1)"
+    });
+    $newdiv.addClass('star');
+    var posx = (Math.random() * ($(window).width() - divsize)).toFixed();
+    var posy = (Math.random() * ($(window).height() - divsize)).toFixed();
+    
+    $newdiv.css({
+        'position':'fixed',
+        'left':posx+'px',
+        'top':posy+'px',
+        'display':'none'
+    }).appendTo( 'body' ).fadeIn(1000).delay(1000).fadeOut(1000, function(){
+       $(this).remove();
+       
+    }); 
+}, 100);
+
+
+
+})
+
+
+
+
+
+
