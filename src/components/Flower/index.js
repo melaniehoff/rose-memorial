40645c5f238316d4664837b42d3df6bdedb24bde
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2021-05-18 11:51:00 -0400

    trying to adjust tabbing a bit...

--- src/components/Flower/index.js
+++ src/components/Flower/index.js
@@ -13,61 +13,52 @@ class Flower extends Component {
     	id : 'hello',
     }
    }
-	renderFlower(){
-			const linkCheck = (fieldsOnRecord) => {
-				const { OptionalLink } = fieldsOnRecord
-				if (!OptionalLink) return ('')
-				else {
-					const prefix = OptionalLink.slice(3)
-					if (prefix !== 'http') return (<a className='small-text' href={`https://${OptionalLink}`}>{OptionalLink}</a>)
-					else return (<a className='small-text' href={OptionalLink}>{OptionalLink}</a>)
-				}
-			}
-	   	const records = [];
-	   	for (var i = this.props.records.length - 1; i >= 0; i--) {
-        console.log(slugify(this.props.records[i].fields.Dedication))
-	   		if((slugify(this.props.records[i].fields.Dedication) == this.props.match.params.id.split('_')[0])|| (slugify(this.props.records[i].fields.DedicationPlace) == this.props.match.params.id.split('_')[0]) || (slugify(this.props.records[i].fields.DedicationThing) == this.props.match.params.id.split('_')[0])){
-	   			records.push(
-		   		 <div key={this.props.records[i].id} id={this.props.records[i].id} className="single-flower" >
-
-		           <div className='flower'>
-				        <div className="trim">
-				          {!this.props.records[i].fields.RoseSVG ? '' :
-				            <img alt={(this.props.records[i].fields.Dedication ||  this.props.records[i].fields.DedicationThing || this.props.records[i].fields.DedicationPlace) + " Flower"} src={this.props.records[i].fields.RoseSVG[0].url}/>
-				          }
-				        </div>
-
-			        </div>
-
-              <div id='download-flower' className="medium-text-link">
-              {!this.props.records[i].fields.RoseSVG ? '' :
-               <a className='medium-text-link' href={this.props.records[i].fields.RoseSVG[0].url} download>download flower</a>
-             }
-             </div>
+  renderFlower(){
+	const linkCheck = (fieldsOnRecord) => {
+		const { OptionalLink } = fieldsOnRecord
+		if (!OptionalLink) return ('')
+		else {
+			const prefix = OptionalLink.slice(3)
+			if (prefix !== 'http') return (<a className='small-text' href={`https://${OptionalLink}`}>{OptionalLink}</a>)
+			else return (<a className='small-text' href={OptionalLink}>{OptionalLink}</a>)
+		}
+	}
+	const records = [];
+	for (var i = this.props.records.length - 1; i >= 0; i--) {
+	  if((slugify(this.props.records[i].fields.Dedication) == this.props.match.params.id.split('_')[0])|| (slugify(this.props.records[i].fields.DedicationPlace) == this.props.match.params.id.split('_')[0]) || (slugify(this.props.records[i].fields.DedicationThing) == this.props.match.params.id.split('_')[0])){
+	  records.push(
+		<div key={this.props.records[i].id} id={this.props.records[i].id} className="single-flower" >
+		<div className='flower'>
+		<div className="trim">
+		{!this.props.records[i].fields.RoseSVG ? '' :
+			 <img alt={(this.props.records[i].fields.Dedication ||  this.props.records[i].fields.DedicationThing || this.props.records[i].fields.DedicationPlace) + " Flower"} src={this.props.records[i].fields.RoseSVG[0].url}/>
+		}
+		</div>
+		</div>
+              	<div id='download-flower' className="medium-text-link">
+              	{!this.props.records[i].fields.RoseSVG ? '' :
+               		<a className='medium-text-link' href={this.props.records[i].fields.RoseSVG[0].url} download>download flower</a>
+             	}
+            	</div>
 
 
-              <div className='flower-info-container'>
-    			       <div className='flower-info'>
-
-                      <p className='flower-dedication large-text'>{this.props.records[i].fields.Dedication}{this.props.records[i].fields.DedicationPlace}{this.props.records[i].fields.DedicationThing}</p>
-
-
-                      <p className='flower-dedication small-text'>{this.props.records[i].fields.OptionalLocation}</p>
-
+              	<div className='flower-info-container'>
+    			<div className='flower-info'>
+                        <p className='flower-dedication large-text'>{this.props.records[i].fields.Dedication}{this.props.records[i].fields.DedicationPlace}{this.props.records[i].fields.DedicationThing}</p>
+                      	<p className='flower-dedication small-text'>{this.props.records[i].fields.OptionalLocation}</p>
                       {!this.props.records[i].fields.OptionalPhoto ? '' :
                       <img alt={this.props.records[i].fields.Dedication + " Photo"} src={this.props.records[i].fields.OptionalPhoto[0].url}/>
                       }
                       <div className='rose-note'><p className='medium-text'><ReactMarkdown  source={this.props.records[i].fields.OptionalNote}/></p></div>
                      {linkCheck(this.props.records[i].fields)}
                       <br></br>
-
                       {!this.props.records[i].fields.OptionalVideoLink ? '' :
                       <VideoEmbed videoUrl={this.props.records[i].fields.OptionalVideoLink}/>
                       }
-			         </div>
+			</div>
              </div>
 
-		         </div>
+		 </div>
 		        )
 	   		}
 
